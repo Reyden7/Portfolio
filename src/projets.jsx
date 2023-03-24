a618fab566498b4df10c0d5332c60a3fd597e8c2
Reyden <quentin.cordiero@neuf.fr>
2023-03-24 09:09:48 +0100

    anime on scroll background color

--- src/projets.jsx
+++ src/projets.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 
 function Projets({ title, imageSrc, description }){
