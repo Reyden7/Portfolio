504b1d1e2c474c854f4e94696c8284edc47f115f
Quentin <quentin.cordiero@neuf.fr>
2023-04-24 14:27:13 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,15 @@ import AVenir from './components/a-venir';
 function App ()	{
   return (
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/menu" element={<Home/>} ></Route>
-      <Route path="/competences" element={<Competences/>} ></Route>
-      <Route path="/hobbie" element={<Hobbie/>}></Route>
-      <Route path="/contact" element={<Contact/>}></Route>
-      <Route path="/etabs" element={<Etabs/>}></Route>
-      <Route path="/revi" element={<Revi/>}></Route>
-      <Route path="/vertiscreen" element={<Vertiscreen/>}></Route>
-      <Route path="/a-venir" element={<AVenir/>}></Route>
+      <Route path="https://reyden7.github.io/Portfolio/" element={<Home/>}></Route>
+      <Route path="https://reyden7.github.io/Portfolio/menu" element={<Home/>} ></Route>
+      <Route path="https://reyden7.github.io/Portfolio/competences" element={<Competences/>} ></Route>
+      <Route path="https://reyden7.github.io/Portfolio/hobbie" element={<Hobbie/>}></Route>
+      <Route path="https://reyden7.github.io/Portfolio/contact" element={<Contact/>}></Route>
+      <Route path="https://reyden7.github.io/Portfolio/etabs" element={<Etabs/>}></Route>
+      <Route path="https://reyden7.github.io/Portfolio/revi" element={<Revi/>}></Route>
+      <Route path="https://reyden7.github.io/Portfolio/vertiscreen" element={<Vertiscreen/>}></Route>
+      <Route path="https://reyden7.github.io/Portfolio/a-venir" element={<AVenir/>}></Route>
       <Route  path="*" element={<Home/>} />
     </Routes>
   )
