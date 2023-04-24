c73abc62ac9389bde9b6f968df36cd46915e9fe8
Quentin <quentin.cordiero@neuf.fr>
2023-04-24 17:19:39 +0200

    link to rout navbarre

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -4,7 +4,7 @@ import Projets from './img/projets.png';
 import Competences from './img/competences.png';
 import Hobbie from './img/hobbies.png';
 import Contact from './img/contact_.png';
-
+import { Link } from 'react-router-dom'; // Importer le composant Link de React Router
 
 
 function Navbare(){
@@ -15,16 +15,16 @@ function Navbare(){
                 
                 <div className=' absolute shadow-2xl nav flex-1  items-center h-20  fixed flex justify-center sm:h-20 md:h-20 lg:h-20 sm:mt-0  sm:w-full md:w-auto lg:w-auto xl:w-auto sm:pb-1'>
                     <div className='sm:w-13' tabIndex="0"> 
-                    <a href='./projets' className=' sm:w-6' data-feather="Projets"><img className= 'sm:w-13 md:w-24' src={Projets}></img></a>
+                    <Link to="./projets"  className=' sm:w-6' data-feather="Projets"><img className= 'sm:w-13 md:w-24' src={Projets}></img></Link>
                     </div>
                     <div className='sm:w-13' tabIndex="0"> 
-                    <a href='./competences' className='' data-feather="Projets"><img className='sm:w-13 md:w-24 md:p-0' src={Competences}></img></a>
+                    <Link to="./competences"  className=' sm:w-6' data-feather="Projets"><img className='sm:w-13 md:w-24 md:p-0' src={Competences}></img></Link>
                     </div>
                     <div className='sm:w-13' tabIndex="0"> 
-                    <a  href='./hobbie'className='' data-feather="Projets"><img className=' sm:w-13 md:w-24' src={Hobbie}></img></a>
+                    <Link to="./hobbie"  className=' sm:w-6' data-feather="Projets"><img className='sm:w-13 md:w-24 md:p-0' src={Hobbie}></img></Link>
                     </div>
                     <div className='sm:w-13' tabIndex="0"> 
-                    <a href='./contact' className=''  data-feather="Projets"><img className=' sm:w-13 md:w-24' src={Contact}></img></a>
+                    <Link to="./contact"  className=' sm:w-6' data-feather="Projets"><img className='sm:w-13 md:w-24 md:p-0' src={Contact}></img></Link>
                     </div>
                 </div>
                 
